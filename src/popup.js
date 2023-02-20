8553fc6942e982993ebec925dbb0ed1b96d5c953
JClark-TAMU <jonathan.clark@tamu.edu>
2023-02-19 20:26:20 -0600

    Huge coding change on e2e tests infrastructure

--- src/popup.js
+++
@@ -1,4 +0,0 @@
-const popup = document.querySelector('#popup');
-const para = document.querySelector('#popuptext');
-
-console.log('popup.js loaded.');
--- tests/popup.test.js
+++ tests/popup.test.js
@@ -1,43 +1,43 @@
 require('dotenv').config();
-const puppeteer = require('puppeteer');
-let browser; let page;
-const path = process.env._PATH;
-const puppeteerArgs = [
-  `--disable-extensions-except=${path}`,
-  `--load-extension=${path}`,
-  '--disable-features=DialMediaRouteProvider',
-];
+const path = require('path');
+const bootstrapExtension = require('puppeteer-test-browser-extension');
 
-describe('SIC Extensions', () => {
-  beforeEach(async function() {
-    browser = await puppeteer.launch({
-      headless: false,
-      args: ['--no-sandbox'],
-      executablePath: process.env.PUPPETEER_EXEC_PATH, // set by docker container
-      slowMo: 200,
-      args: puppeteerArgs,
-    });
-    [page] = await browser.pages();
-  });
+describe('Test browser extension', () => {
+	let browser, contentPage, extensionPage;
 
-  afterEach(() => browser.close());
+	beforeAll(async () => {
+		const extensionEnvironment = await bootstrapExtension({
+			pathToExtension: path.resolve('./SIC',''), //The path to the uncompressed extension's folder. It shouldn't be a ZIP file.
+			contentUrl: `https://google.com/`, // The URL of the content page that is being browsed
+			//slowMo: 100, //(uncomment this line to slow down Puppeteer's actions)
+			//devtools: true, //(uncomment this line to open the browser's devtools)
+		});
 
-  it('displays popup', (async () => {
-    const appPage = await browser.newPage();
+		browser = extensionEnvironment.browser;
+		contentPage = extensionEnvironment.contentPage;
+		extensionPage = extensionEnvironment.extensionPage;
+	});
 
-    const targets = await browser.targets();
-    const extensionTarget = targets.find(
-        (target) => target.type() === 'service_worker');
-    const partialExtensionUrl = extensionTarget.url() || '';
-    const [, , extensionId] = partialExtensionUrl.split('/');
-    console.log(extensionId);
+	it("Should open the extension's popup", async () => {
+		// Use contentPage to interact with the content page (the page that is being browsed)
+		// First, activate the content page
+		contentPage.bringToFront();
 
-    const extensionUrl = `chrome-extension://${extensionId}/popup.html`;
+		// You can use Puppeteer's features as usual
+		//Example: Click the button
 
-    await appPage.goto(
-        extensionUrl, {waitUntil: ['domcontentloaded', 'networkidle2']});
+		// Use extensionPage to interact with the extension's popup
+		// First, activate the popup
+		await extensionPage.bringToFront();
 
-    const popupHeading = await appPage.$eval('h2', ((e) => e.innerText));
-    expect(popupHeading).toEqual('SIC Extension');
-  }));
+		// (Assuming your content page contains <h1>Extension popup</h1>)
+		// The user should see the heading on the popup
+		const heading = await extensionPage.$('h2');
+		const extensionHeadingText = await heading.evaluate((e) => e.innerText);
+		expect(extensionHeadingText).toEqual('SIC Extension');
+	});
+
+	afterAll(async () => {
+		await browser.close();
+	});
 });
