429b0bb0957195c40d82e4952aa3bc788a5e194c
JClark-TAMU <jonathan.clark@tamu.edu>
2023-02-19 21:03:27 -0600

    SIC folder to popup

--- tests/popup.test.js
+++ tests/popup.test.js
@@ -7,7 +7,7 @@ describe('Test browser extension', () => {
 
 	beforeAll(async () => {
 		const extensionEnvironment = await bootstrapExtension({
-			pathToExtension: path.resolve('./SIC',''), //The path to the uncompressed extension's folder. It shouldn't be a ZIP file.
+			pathToExtension: path.resolve('./popup',''), //The path to the uncompressed extension's folder. It shouldn't be a ZIP file.
 			contentUrl: `https://google.com/`, // The URL of the content page that is being browsed
 			//slowMo: 100, //(uncomment this line to slow down Puppeteer's actions)
 			//devtools: true, //(uncomment this line to open the browser's devtools)
