4dc0ed1bf9d815a1136f86ef16e82dd33764e9bf
erntv <90066267+erntv@users.noreply.github.com>
2023-02-21 16:43:37 -0600

    Makes code style changes

--- background.js
+++ background.js
@@ -1,25 +1,25 @@
 // listen for 'messages' from extension and content scripts
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.debug("Extension recieved message", request);
+  console.debug('Extension recieved message', request);
 
-    let is_from_ext = chrome.runtime.id == sender.id;
-    let is_valid = "msg_type" in request && "msg_content" in request;
+  const isFromExt = chrome.runtime.id == sender.id;
+  const isValid = 'msg_type' in request && 'msg_content' in request;
 
-    // handle message that has topics list
-    if (is_from_ext && is_valid && "list" in request.msg_content && request.msg_type == "first_save_topics" && request.msg_content.list.length >= 1) {
-        console.debug("Message has the topics list");
+  // handle message that has topics list
+  if (isFromExt && isValid &&
+    'list' in request.msg_content &&
+    request.msg_type == 'first_save_topics' &&
+    request.msg_content.list.length >= 1) {
+    console.debug('Message has the topics list');
 
-        // do something with topic list
-        console.debug("Topics list is", request.msg_content.list);
+    // do something with topic list
+    console.debug('Topics list is', request.msg_content.list);
 
-        // send reply back to popup
-        sendResponse({ status: "ok" });
-    }
-
+    // send reply back to popup
+    sendResponse({status: 'ok'});
+  } else {
     // ignore all other messages
-    else {
-        console.error("Message is invalid");
-        sendResponse({ status: "failed" });
-    }
-}
-);
+    console.error('Message is invalid');
+    sendResponse({status: 'failed'});
+  }
+});
--- popup/popup.js
+++ popup/popup.js
@@ -1,94 +1,96 @@
 /**
  * Returns the element representation of the id.
- * @param {string} element_id 
- * @returns {HTMLElement}
+ * @param {string} elementId
+ * @return {HTMLElement}
  */
-function dom_id(element_id) {
-    return document.getElementById(element_id);
+function getElementFromId(elementId) {
+  return document.getElementById(elementId);
 }
 
 /**
  * Shows the provided section element.
  * @param {HTMLElement} element - the element to show
  */
-function show_section(element) {
-    console.debug("Showing", element);
-    element.style.display = "block";
+function showSection(element) {
+  console.debug('Showing', element);
+  element.style.display = 'block';
 }
 
 /**
  * Hides the provided section element.
  * @param {HTMLElement} element - the element to hide
  */
-function hide_section(element) {
-    console.debug("Hiding", element);
-    element.style.display = "none";
+function hideSection(element) {
+  console.debug('Hiding', element);
+  element.style.display = 'none';
 }
 
 /**
  * Adds a word to the display element.
  * @param {string} word - the word to add
  */
-function add_word_to_display(word) {
-    let element = document.createElement("span");
-    element.innerText = word;
-    topics_list.appendChild(element);
-    console.debug("Added topics list display:", word);
+function addWordToDisplay(word) {
+  const element = document.createElement('span');
+  element.innerText = word;
+  topicsList.appendChild(element);
+  console.debug('Added topics list display:', word);
 }
 
 /**
- * Sends a message to the extension background with the final list of topics the user chose.
+ * Sends a message to the extension with the list of topics the user chose.
  */
-function send_list_to_backend() {
-    console.debug("Popup will send sensitive topics list to extension for processing");
-    (async () => {
-        const response = await chrome.runtime.sendMessage({
-            msg_type: "first_save_topics",
-            msg_content: { list: restricted_words }
-        });
-        console.debug("Popup recieved acknowledgement", response);
-        if (response.status == "ok") {
-            hide_section(dom_id("section-choice"));
-            hide_section(dom_id("section-welcome"));
-            show_section(dom_id("section-enabled"));
-        } else {
-            console.error("Extension did not acknowledge message");
-        }
-    })();
+function sendListToBackend() {
+  console.debug('Popup will send sensitive topics list to extension');
+  (async () => {
+    const response = await chrome.runtime.sendMessage({
+      msg_type: 'first_save_topics',
+      msg_content: {list: restrictedWords},
+    });
+    console.debug('Popup recieved acknowledgement', response);
+    if (response.status == 'ok') {
+      hideSection(getElementFromId('section-choice'));
+      hideSection(getElementFromId('section-welcome'));
+      showSection(getElementFromId('section-enabled'));
+    } else {
+      console.error('Extension did not acknowledge message');
+    }
+  })();
 }
 
 // example restricted words
-let restricted_words = ["violence", "segmentation fault", "pain"];
-console.info("Initial sensitive topics", restricted_words);
+const restrictedWords = ['violence', 'segmentation fault', 'pain'];
+console.info('Initial sensitive topics', restrictedWords);
 
 // element that represents the list of topics
-const topics_list = document.getElementsByClassName("container-words")[0];
+const topicsList = document.getElementsByClassName('container-words')[0];
 
 // add all words to display element
-restricted_words.forEach((word) => { add_word_to_display(word); });
+restrictedWords.forEach((word) => {
+  addWordToDisplay(word);
+});
 
 // 'Set up custom topics' button shows the topic list screen
-dom_id("button-continue-setup").addEventListener("click", () => {
-    hide_section(dom_id("section-welcome"));
-    show_section(dom_id("section-choice"));
+getElementFromId('button-continue-setup').addEventListener('click', () => {
+  hideSection(getElementFromId('section-welcome'));
+  showSection(getElementFromId('section-choice'));
 });
 
 // 'Use defaults' button completes setup
-dom_id("button-default-setup").addEventListener("click", () => {
-    console.debug("User chose to use default configuration");
-    send_list_to_backend();
+getElementFromId('button-default-setup').addEventListener('click', () => {
+  console.debug('User chose to use default configuration');
+  sendListToBackend();
 });
 
 // 'Finish setup' button on topics screen completes setup
-dom_id("button-finish-setup").addEventListener("click", () => {
-    console.debug("User clicked finish setup button");
-    send_list_to_backend();
+getElementFromId('button-finish-setup').addEventListener('click', () => {
+  console.debug('User clicked finish setup button');
+  sendListToBackend();
 });
 
 // 'Add' button on topics screen adds topic to list
-dom_id("button-add-word").addEventListener("click", () => {
-    let input_value = dom_id("section-choice-input").value;
-    add_word_to_display(input_value);
-    restricted_words.push(input_value);
-    dom_id("section-choice-input").value = "";
+getElementFromId('button-add-word').addEventListener('click', () => {
+  const inputValue = getElementFromId('section-choice-input').value;
+  addWordToDisplay(inputValue);
+  restrictedWords.push(inputValue);
+  getElementFromId('section-choice-input').value = '';
 });
