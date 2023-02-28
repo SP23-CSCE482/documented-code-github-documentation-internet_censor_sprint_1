51e8264b0e0b9a5e8b4f4b962b7aa49abc4762a3
erntv <90066267+erntv@users.noreply.github.com>
2023-02-27 19:39:56 -0600

    Changes test location

--- tests/popup.test.js
+++ tests/popup.test.js
@@ -3,41 +3,38 @@ const path = require('path');
 const bootstrapExtension = require('puppeteer-test-browser-extension');
 
 describe('Test browser extension', () => {
-	let browser, contentPage, extensionPage;
+  let browser; let contentPage; let extensionPage;
 
-	beforeAll(async () => {
-		const extensionEnvironment = await bootstrapExtension({
-			pathToExtension: path.resolve('./popup',''), //The path to the uncompressed extension's folder. It shouldn't be a ZIP file.
-			contentUrl: `https://google.com/`, // The URL of the content page that is being browsed
-			//slowMo: 100, //(uncomment this line to slow down Puppeteer's actions)
-			//devtools: true, //(uncomment this line to open the browser's devtools)
-		});
+  beforeAll(async () => {
+    const extensionEnvironment = await bootstrapExtension({
+      pathToExtension: path.resolve('./tests/popup', ''),
+      contentUrl: 'https://google.com/',
+    });
 
-		browser = extensionEnvironment.browser;
-		contentPage = extensionEnvironment.contentPage;
-		extensionPage = extensionEnvironment.extensionPage;
-	});
+    browser = extensionEnvironment.browser;
+    contentPage = extensionEnvironment.contentPage;
+    extensionPage = extensionEnvironment.extensionPage;
+  });
 
-	it("Should open the extension's popup", async () => {
-		// Use contentPage to interact with the content page (the page that is being browsed)
-		// First, activate the content page
-		contentPage.bringToFront();
+  it('Should open the extension\'s popup', async () => {
+    // First, activate the content page
+    contentPage.bringToFront();
 
-		// You can use Puppeteer's features as usual
-		//Example: Click the button
+    // You can use Puppeteer's features as usual
+    // Example: Click the button
 
-		// Use extensionPage to interact with the extension's popup
-		// First, activate the popup
-		await extensionPage.bringToFront();
+    // Use extensionPage to interact with the extension's popup
+    // First, activate the popup
+    await extensionPage.bringToFront();
 
-		// (Assuming your content page contains <h1>Extension popup</h1>)
-		// The user should see the heading on the popup
-		const heading = await extensionPage.$('span');
-		const extensionHeadingText = await heading.evaluate((e) => e.innerText);
-		expect(extensionHeadingText).toEqual('Safe Internet');
-	});
+    // (Assuming your content page contains <h1>Extension popup</h1>)
+    // The user should see the heading on the popup
+    const heading = await extensionPage.$('span');
+    const extensionHeadingText = await heading.evaluate((e) => e.innerText);
+    expect(extensionHeadingText).toEqual('Safe Internet');
+  });
 
-	afterAll(async () => {
-		await browser.close();
-	});
+  afterAll(async () => {
+    await browser.close();
+  });
 });
