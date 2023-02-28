81adb9c4d3d3990d20a18c266b19faf51d396645
JClark-TAMU <jonathan.clark@tamu.edu>
2023-02-28 16:27:42 -0600

    Modified Parser and finished testing

--- tests/parser.test.js
+++ tests/parser.test.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const parser = require('../popup/parser.js');
 const path = require('path');
 const bootstrapExtension = require('puppeteer-test-browser-extension');
 
@@ -8,7 +9,7 @@ describe('Test browser extension', () => {
 	beforeAll(async () => {
 		const extensionEnvironment = await bootstrapExtension({
 			pathToExtension: path.resolve('./popup',''), //The path to the uncompressed extension's folder. It shouldn't be a ZIP file.
-			contentUrl: `https://google.com/`, // The URL of the content page that is being browsed
+			contentUrl: `https://rateyourmusic.com/`, // The URL of the content page that is being browsed
 			//slowMo: 100, //(uncomment this line to slow down Puppeteer's actions)
 			//devtools: true, //(uncomment this line to open the browser's devtools)
 		});
@@ -22,23 +23,23 @@ describe('Test browser extension', () => {
 		await browser.close();
 	});
 
-	it("Should open the extension's popup", async () => {
+	it("Should return Pure Text", async () => {
 		// Use contentPage to interact with the content page (the page that is being browsed)
 		// First, activate the content page
 		contentPage.bringToFront();
+        const bodyHTML = await contentPage.content();
+        const bodyText = parser.parsePage(bodyHTML);
+		expect(bodyText).not.toMatch('<');
+		expect(bodyText).not.toMatch('>');
+	});
 
-		// You can use Puppeteer's features as usual
-		//Example: Click the button
-
-		// Use extensionPage to interact with the extension's popup
-		// First, activate the popup
-		await extensionPage.bringToFront();
-
-		// (Assuming your content page contains <h1>Extension popup</h1>)
-		// The user should see the heading on the popup
-		const heading = await extensionPage.$('span');
-		const extensionHeadingText = await heading.evaluate((e) => e.innerText);
-		expect(extensionHeadingText).toEqual('Safe Internet');
+	it("Should expect the title of the website", async () => {
+		// Use contentPage to interact with the content page (the page that is being browsed)
+		// First, activate the content page
+		contentPage.bringToFront();
+        const bodyHTML = await contentPage.content();
+        const bodyText = parser.parsePage(bodyHTML);
+		expect(bodyText).toMatch('Rate Your Music');
 	});
 
 });
