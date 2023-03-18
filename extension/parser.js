f92c5625fd95650b49e965174d7251677c9929e5
JClark-TAMU <jonathan.clark@tamu.edu>
2023-03-18 18:34:53 -0500

    Revised file hierachy for testing

---
+++ extension/parser.js
@@ -0,0 +1,14 @@
+const { htmlToText } = require('html-to-text');
+
+// Parses html page string
+// Input: 
+// page -- html page as a string
+// Output:
+// returns the pure text of the html page
+const parser = {
+    parsePage: function parsePage(page) {
+        return htmlToText(page);
+    }
+}
+module.exports = parser;
+//console.log(typeof parser.parsePage("<!DOCTYPE html><head> this thing </head><body><p>Hello world</p><p>This is violence incarnate.</p></body>"));
