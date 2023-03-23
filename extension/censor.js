19866d502bf06058896557e6c4eab2f93ae632dd
Zachary-Fournier <zachary4nier@gmail.com>
2023-03-23 08:56:04 -0500

    Updated censor for dynamic pages
    The censor works for dynamic pages such as twitter. Currently disabled on google searches as there is a bug

--- extension/censor.js
+++ extension/censor.js
@@ -2,7 +2,7 @@
 function censorKeywords(keywords) {
   // Get all the text nodes on the web page that are not part of a script or style element.
   const textNodes = document.evaluate(
-    '//text()[not(ancestor::script)][not(ancestor::style)]',
+    '//text()[not(ancestor::script)][not(ancestor::style)][not(ancestor::input)]',
     document,
     null,
     XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,
@@ -17,8 +17,13 @@ function censorKeywords(keywords) {
     const node = textNodes.snapshotItem(i);
     const textContent = node.textContent;
 
+    // Check if the text node is part of Google search result elements.
+    const isInGoogleSearchElement =
+      node.parentElement.closest('div[data-async-context]') ||
+      node.parentElement.closest('div[id="searchform"]');
+
     // Check if the text node contains any of the keywords to be censored.
-    if (regex.test(textContent)) {
+    if (!isInGoogleSearchElement && regex.test(textContent)) {
       // Replace the keywords with redacted elements.
       const newText = textContent.replace(regex, (match) => {
         const span = document.createElement('span');
@@ -35,6 +40,20 @@ function censorKeywords(keywords) {
   }
 }
 
+// Observe DOM changes and re-run the censor function if any changes are detected.
+function observeDOMChanges(keywords) {
+  const observer = new MutationObserver(() => {
+    censorKeywords(keywords);
+  });
+
+  observer.observe(document.body, {
+    childList: true,
+    subtree: true,
+  });
+
+  return observer;
+}
+
 // Function to fetch keywords from chrome.storage and call the censorKeywords function.
 chrome.storage.local.get('keywords', function (result) {
   // Check if keywords are available in the storage.
@@ -43,8 +62,17 @@ chrome.storage.local.get('keywords', function (result) {
     const keywords = Object.keys(result.keywords).filter(
       (keyword) => result.keywords[keyword]
     );
+
     // Call the censorKeywords function with the fetched keywords.
     censorKeywords(keywords);
+
+    // Observe DOM changes and re-run the censor function when necessary.
+    const observer = observeDOMChanges(keywords);
+
+    // Disconnect the observer when the window is unloaded.
+    window.addEventListener('unload', () => {
+      observer.disconnect();
+    });
   } else {
     console.error('No keywords found in storage');
   }
