43d0bb853abf3b0707f131e90ec33f3a0527a416
Zachary-Fournier <zachary4nier@gmail.com>
2023-03-24 16:17:19 -0500

    linter fixes

--- extension/censor.js
+++ extension/censor.js
@@ -50,13 +50,12 @@ function censorKeywords(keywords) {
  */
 function debounce(func, wait) {
   let timeout;
-  return function (...args) {
-    const context = this;
+  return function debounced(...args) {
     clearTimeout(timeout);
     timeout = setTimeout(() => {
-      func.apply(context, args);
+      func.apply(debounced, args);
     }, wait);
-  };
+  }.bind(this);
 }
 
 
