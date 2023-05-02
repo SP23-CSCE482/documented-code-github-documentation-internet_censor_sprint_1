6887ff989add7aebc23d98463b102d0dc16629ca
Zachary Fournier <zachary4nier@gmail.com>
2023-05-01 19:37:17 -0500

    Passing lint tests

--- extension/toxicity.js
+++ extension/toxicity.js
@@ -1,42 +1,46 @@
 import * as toxicity from '@tensorflow-models/toxicity';
 
-
+/**
+ * Loads and handles the toxicity model.
+ * @exports ToxicityClassifier
+ */
 export class ToxicityClassifier {
-    static TOXICITY_MODEL;
-    static IS_MODEL_LOADED = false;
-    static TOXICITY_THRESHOLD = 0.1;
-    static RECOMMENDED_MAX_BATCH_SIZE = 30;
+  static TOXICITY_MODEL;
+  static IS_MODEL_LOADED = false;
+  static TOXICITY_THRESHOLD = 0.1;
+  static RECOMMENDED_MAX_BATCH_SIZE = 30;
 
-    // load model
-    static {
-        toxicity.load(this.TOXICITY_THRESHOLD).then(model => {
-            this.TOXICITY_MODEL = model;
-            this.IS_MODEL_LOADED = true;
-        });
-    }
+  // load model
+  static {
+    toxicity.load(this.TOXICITY_THRESHOLD).then((model) => {
+      this.TOXICITY_MODEL = model;
+      this.IS_MODEL_LOADED = true;
+    });
+  }
 
-    /**
+  /**
      * Returns object with different metrics on sentiment of input
      * @param {string} inputString The string to evaluate
-     * @returns {*}
+     * @return {*}
      */
-    static async isToxic(inputString) {
-        if (!this.IS_MODEL_LOADED) { throw new Error("Model not loaded."); }
-        let prediction = await this.TOXICITY_MODEL.classify([inputString]);
-        return prediction;
+  static async isToxic(inputString) {
+    if (!this.IS_MODEL_LOADED) {
+      throw new Error('Model not loaded.');
     }
+    const prediction = await this.TOXICITY_MODEL.classify([inputString]);
+    return prediction;
+  }
 
-    /**
+  /**
      * Get the toxicity by batch
-     * @param {*} inputArray 
-     * @returns {*}
+     * @param {*} inputArray
+     * @return {*}
      */
-    static async getToxicityByBatch(inputArray) {
-        if (inputArray.length > this.RECOMMENDED_MAX_BATCH_SIZE) {
-            console.warning("Batch size is larger than " + this.RECOMMENDED_MAX_BATCH_SIZE + " elements");
-        }
-        let prediction = await this.TOXICITY_MODEL.classify(inputArray);
-        return prediction;
+  static async getToxicityByBatch(inputArray) {
+    if (inputArray.length > this.RECOMMENDED_MAX_BATCH_SIZE) {
+      console.warning('Batch size is larger than ' + this.RECOMMENDED_MAX_BATCH_SIZE + ' elements');
     }
-
+    const prediction = await this.TOXICITY_MODEL.classify(inputArray);
+    return prediction;
+  }
 }
